33b6f497b648fff56f75307bf3f050be8f70a3a8
hoeken <hoeken@4a735864-2852-0410-872d-5d94fdcbaaf2>
2008-08-13 14:42:55 +0000

    updated windows/mac build scripts.

--- trunk/build/windows/dist.sh
+++ trunk/build/windows/dist.sh
@@ -17,93 +17,93 @@ echo Creating ReplicatorG distribution for revision $REVISION...
 echo
 
 # remove any old boogers
-rm -rf ReplicatorG
-rm -rf ReplicatorG-*
+rm -rf replicatorg
+rm -rf replicatorg-*
 
 # use 'shared' files as starting point
-cp -r ../shared ReplicatorG
-#mkdir ReplicatorG
-#cp -r work/lib/targets ReplicatorG/lib
+cp -r ../shared replicatorg
+#mkdir replicatorg
+#cp -r work/lib/targets replicatorg/lib
 
 # new style examples thing ala reas
-#cd ReplicatorG
+#cd replicatorg
 #mkdir examples
 #unzip -d examples -q dist/examples.zip
 #rm dist/examples.zip
 #rm -rf dist
 #cd ..
 
-rm -rf ReplicatorG/dist
+rm -rf replicatorg/dist
 
 # extract reference
-#cd ReplicatorG
+#cd replicatorg
 #unzip reference.zip
 #rm reference.zip
 #cd ..
 
 # add java (jre) files
-unzip -q -d ReplicatorG jre.zip
+unzip -q -d replicatorg jre.zip
 
 # copy stuff from work/
-cp work/readme.txt ReplicatorG
-cp -r work/drivers ReplicatorG
-#cp -r work/examples ReplicatorG
+cp work/readme.txt replicatorg
+cp -r work/drivers replicatorg
+#cp -r work/examples replicatorg
 
 # directories used by the app
-#mkdir ReplicatorG/lib/build
+#mkdir replicatorg/lib/build
 
 # grab pde.jar and export from the working dir
-cp work/lib/pde.jar ReplicatorG/lib/
-cp work/java/lib/rt.jar ReplicatorG/lib/
-#cp work/lib/core.jar ReplicatorG/lib/
-#cp -r work/lib/export ReplicatorG/lib/
-#rm -rf ReplicatorG/lib/export/CVS
+cp work/lib/pde.jar replicatorg/lib/
+cp work/java/lib/rt.jar replicatorg/lib/
+#cp work/lib/core.jar replicatorg/lib/
+#cp -r work/lib/export replicatorg/lib/
+#rm -rf replicatorg/lib/export/CVS
 
 # get jikes and depedencies
-#gunzip < dist/jikes.gz > ReplicatorG/jikes.exe
-cp dist/jikes.exe ReplicatorG/
-chmod +x ReplicatorG/jikes.exe
+#gunzip < dist/jikes.gz > replicatorg/jikes.exe
+cp dist/jikes.exe replicatorg/
+chmod +x replicatorg/jikes.exe
 
-#cp dist/ICE_JNIRegistry.dll ReplicatorG/
-#chmod +x ReplicatorG/ICE_JNIRegistry.dll
-#cp work/rxtxSerial.dll ReplicatorG/
-#chmod +x ReplicatorG/rxtxSerial.dll
-cp work/*.dll ReplicatorG
-chmod +x ReplicatorG/*.dll
+#cp dist/ICE_JNIRegistry.dll replicatorg/
+#chmod +x replicatorg/ICE_JNIRegistry.dll
+#cp work/rxtxSerial.dll replicatorg/
+#chmod +x replicatorg/rxtxSerial.dll
+cp work/*.dll replicatorg
+chmod +x replicatorg/*.dll
 
 # get platform-specific goodies from the dist dir
-cp launcher/ReplicatorG.exe ReplicatorG/
-cp dist/run.bat ReplicatorG/
-chmod +x ReplicatorG/run.bat
+cp launcher/ReplicatorG.exe replicatorg/
+cp dist/run.bat replicatorg/
+chmod +x replicatorg/run.bat
 
 # convert notes.txt to windows LFs
 # the 2> is because the app is a little chatty
-unix2dos ReplicatorG/revisions.txt 2> /dev/null
-unix2dos ReplicatorG/lib/preferences.txt 2> /dev/null
-unix2dos ReplicatorG/lib/keywords.txt 2> /dev/null
-rm -f ReplicatorG/*.bak
-rm -f ReplicatorG/lib/*.bak
+unix2dos replicatorg/revisions.txt 2> /dev/null
+unix2dos replicatorg/lib/preferences.txt 2> /dev/null
+unix2dos replicatorg/lib/keywords.txt 2> /dev/null
+rm -f replicatorg/*.bak
+rm -f replicatorg/lib/*.bak
 
 # remove boogers
-find ReplicatorG -name "*~" -exec rm -f {} ';'
-find ReplicatorG -name ".DS_Store" -exec rm -f {} ';'
-find ReplicatorG -name "._*" -exec rm -f {} ';'
-find ReplicatorG -name "Thumbs.db" -exec rm -f {} ';'
+find replicatorg -name "*~" -exec rm -f {} ';'
+find replicatorg -name ".DS_Store" -exec rm -f {} ';'
+find replicatorg -name "._*" -exec rm -f {} ';'
+find replicatorg -name "Thumbs.db" -exec rm -f {} ';'
 
 # chmod +x the crew
-find ReplicatorG -name "*.dll" -exec chmod +x {} ';'
-find ReplicatorG -name "*.exe" -exec chmod +x {} ';'
-find ReplicatorG -name "*.html" -exec chmod +x {} ';'
+find replicatorg -name "*.dll" -exec chmod +x {} ';'
+find replicatorg -name "*.exe" -exec chmod +x {} ';'
+find replicatorg -name "*.html" -exec chmod +x {} ';'
 
 # clean out the svn entries
-find ReplicatorG -name ".svn" -exec rm -rf {} ';' 2> /dev/null
+find replicatorg -name ".svn" -exec rm -rf {} ';' 2> /dev/null
 
 # zip it all up for release
 echo Packaging standard release...
 echo
-P5=ReplicatorG-$REVISION
-mv ReplicatorG $P5
-zip -rq $P5.zip $P5
+P5=replicatorg-$REVISION
+mv replicatorg $P5
+zip -rq $P5-windows.zip $P5
 # nah, keep the new directory around
 #rm -rf $P5
 
