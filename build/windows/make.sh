77786a9fe3698eaa2b052e3b5b89a2b398c32eb6
hoeken <hoeken@4a735864-2852-0410-872d-5d94fdcbaaf2>
2008-10-17 18:02:13 +0000

    switched to using jdk to build and not having a bundled JRE.  will it work?

--- build/windows/make.sh
+++ build/windows/make.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+#JAVA_OPTIONS="-Xlint:deprecation"
+JAVA_OPTIONS="" 
 
 ### -- SETUP WORK DIR -------------------------------------------
 
@@ -66,14 +68,21 @@ cd ../..
 
 cd app
 
-CLASSPATH="..\\build\\windows\\work\\lib\\vecmath.jar;..\\build\\windows\\work\\lib\\j3dcore.jar;..\\build\\windows\\work\\lib\\j3dutils.jar;..\\build\\windows\\work\\lib\\RXTXcomm.jar;..\\build\\windows\\work\\lib\\mrj.jar;..\\build\\windows\\work\\lib\antlr.jar;..\\build\\windows\\work\\lib\\oro.jar;..\\build\\windows\\work\\lib\\registry.jar;..\\build\\windows\\work\\java\\lib\\rt.jar"
+JAVA_BIN="/cygdrive/f/Program Files/Java/jdk1.6.0_10/bin";
+PATH="$PATH:$JAVA_BIN"
+echo $PATH;
+
+#CLASSPATH=dirname `which javaw.exe`
+CLASSPATH="..\\build\\windows\\work\\lib\\vecmath.jar;..\\build\\windows\\work\\lib\\j3dcore.jar;..\\build\\windows\\work\\lib\\j3dutils.jar;..\\build\\windows\\work\\lib\\RXTXcomm.jar;..\\build\\windows\\work\\lib\\mrj.jar;..\\build\\windows\\work\\lib\antlr.jar;..\\build\\windows\\work\\lib\\oro.jar;..\\build\\windows\\work\\lib\\registry.jar;..\\build\\windows\\work\\classes"
 
 # compile the code as java 1.3, so that the application will run and
 # show the user an error, rather than crapping out with some strange
 # "class not found" crap
 # need to do this twice because otherwise dependencies aren't resolved right.
-../build/windows/work/jikes -target 1.3 +D -classpath "$CLASSPATH;..\\build\\windows\\work\\classes" -d ..\\build\\windows\\work\\classes ../core/*.java drivers/*.java syntax/*.java exceptions/*.java models/*.java tools/*.java *.java
-../build/windows/work/jikes -target 1.3 +D -classpath "$CLASSPATH;..\\build\\windows\\work\\classes" -d ..\\build\\windows\\work\\classes ../core/*.java drivers/*.java syntax/*.java exceptions/*.java models/*.java tools/*.java *.java
+#../build/windows/work/jikes -target 1.3 +D -classpath "$CLASSPATH;..\\build\\windows\\work\\classes" -d ..\\build\\windows\\work\\classes ../core/*.java drivers/*.java syntax/*.java exceptions/*.java models/*.java tools/*.java *.java
+#../build/windows/work/jikes -target 1.3 +D -classpath "$CLASSPATH;..\\build\\windows\\work\\classes" -d ..\\build\\windows\\work\\classes ../core/*.java drivers/*.java syntax/*.java exceptions/*.java models/*.java tools/*.java *.java
+#javac -classpath "$CLASSPATH" -d ..\\build\\windows\\work\\classes ../core/*.java drivers/*.java syntax/*.java exceptions/*.java models/*.java tools/*.java *.java
+javac -source 1.4 -target 1.4 $JAVA_OPTIONS -classpath $CLASSPATH -d ../build/windows/work/classes drivers/*.java models/*.java exceptions/*.java ../core/*.java tools/*.java syntax/*java *.java
 
 cd ../build/windows/work/classes
 rm -f ../lib/pde.jar
@@ -81,8 +90,6 @@ zip -0rq ../lib/pde.jar .
 
 # back to build/windows
 cd ../..
-
-
 
 echo
 echo Done.
