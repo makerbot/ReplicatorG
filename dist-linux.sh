2382ba02a8f1d96703f7e42e732d4976b2896449
hoeken <hoeken@4a735864-2852-0410-872d-5d94fdcbaaf2>
2009-04-17 03:26:38 +0000

    tweaked the build/dist process a bunch

--- dist-linux.sh
+++ dist-linux.sh
@@ -5,4 +5,4 @@ REVISION=`head -n 1 todo.txt | sed 's/[^0-9]//g'`
 
 rm -rf dist
 ant clean
-ant -Dreplicatorg.version=$REVISION -lib jarbundler-1.8.1.jar dist
+ant -Dreplicatorg.version=$REVISION dist-linux
---
+++ dist-mac.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+#REVISION=`head -c 4 todo.txt`
+REVISION=`head -n 1 todo.txt | sed 's/[^0-9]//g'`
+
+rm -rf dist
+ant clean
+ant -Dreplicatorg.version=$REVISION -lib jarbundler-1.8.1.jar dist-macosx
