7f6b7be7f7c7a576e928b16e9d9fea1d06037f9e
hoeken <hoeken@4a735864-2852-0410-872d-5d94fdcbaaf2>
2009-04-17 03:02:15 +0000

    added the latest version of skeinforge to the repository.

---
+++ src/skeinforge/show_skeinforge.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+#
+# Utility script for keeping track of which preference settings a file was processed
+# with, by copying the current preferences to a date-tagged directory together
+# with the output files.
+#
+# Usage: runskeinforge.sh <model.gts>
+#
+import skeinforge
+skeinforge.main()
