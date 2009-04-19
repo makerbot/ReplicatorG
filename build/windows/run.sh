0bf80cdd44e2513524a4859344331632a3a16709
hoeken <hoeken@4a735864-2852-0410-872d-5d94fdcbaaf2>
2009-04-19 01:09:20 +0000

    removed a bunch of stuff.

--- build/windows/run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-PATH=lib:$PATH
-CLASSPATH=\"java\\lib\\rt.jar\;lib\\vecmath.jar\;lib\\j3dcore.jar\;lib\\j3dutils.jar\;lib\\RXTXcomm.jar\;lib\;lib\\build\;lib\\pde.jar\;lib\\mrj.jar\;lib\\antlr.jar\;lib\\oro.jar\;lib\\registry.jar\"
-export PATH
-export CLASSPATH
-
-cd work && java processing.app.Base
-#cd work && /cygdrive/c/jdk-1.3.1_11/bin/java PdeBase
