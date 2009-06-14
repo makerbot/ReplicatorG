6494708046859a31a4a2e7d0c137afaf35583db7
phooky <phooky@4a735864-2852-0410-872d-5d94fdcbaaf2>
2009-06-14 22:31:45 +0000

    added missing version and exception objects

---
+++ src/replicatorg/app/drivers/BadFirmwareVersionException.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package replicatorg.app.drivers;
+
+/**
+ * Indicate that initialization failed because the machine is using obsolete firmware.
+ * This should be used to indicate that the firmware version is absolutely unusable
+ * with the current driver.
+ * @author phooky
+ *
+ */
+public class BadFirmwareVersionException extends RuntimeException {
+	private static final long serialVersionUID = 6973397918493070849L;
+	/** The firmware version that was detected */
+	Version has;
+	/** The firmware version that is needed */
+	Version needs;
+	
+	public BadFirmwareVersionException(Version has, Version needs) {
+		super("Firmware v"+has+" detected; firmware v"+needs+" required.");
+		this.has = has;
+		this.needs = needs;
+	}
+	
+	public Version getHas() { return has; }
+	
+	public Version getNeeds() { return needs; }
+	
+}
