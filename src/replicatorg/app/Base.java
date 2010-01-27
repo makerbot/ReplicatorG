7c85cdd189f0a31d3a126c54027eb0cdf7fcfa74
Adam Mayer <adam@makerbot.com>
2010-01-27 16:41:35 -0500

    updated version

--- src/replicatorg/app/Base.java
+++ src/replicatorg/app/Base.java
@@ -88,7 +88,7 @@ public class Base {
 	/**
 	 * The version number of this edition of replicatorG.
 	 */
-	public static final int VERSION = 11;
+	public static final int VERSION = 12;
 	/**
 	 * The textual representation of this version (4 digits, zero padded).
 	 */
