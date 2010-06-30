8205156c2b1135bd62eac02425d8fd53f775301e
phooky <adam@makerbot.com>
2010-06-30 10:22:56 -0400

    * Added STL writer

---
+++ src/replicatorg/model/j3d/StlAsciiWriter.java
@@ -0,0 +1,49 @@
+package replicatorg.model.j3d;
+
+import java.io.OutputStream;
+import java.io.PrintWriter;
+
+import javax.media.j3d.Shape3D;
+import javax.media.j3d.Transform3D;
+import javax.media.j3d.TriangleArray;
+
+import replicatorg.app.Base;
+
+public class StlAsciiWriter extends ModelWriter {
+	public StlAsciiWriter(OutputStream ostream) {
+		super(ostream);
+	}
+	
+	@Override
+	public void writeShape(Shape3D shape, Transform3D transform) {
+		PrintWriter w = new PrintWriter(ostream);
+		TriangleArray g = getGeometry(shape);
+		if (g == null) {
+			Base.logger.info("Couldn't find valid geometry during save.");
+			return;
+		}
+		String name = shape.getName();
+		if (name == null) { name = "Default"; }
+		
+		w.printf("solid %s\n", name);
+		int faces = g.getVertexCount()/3;
+		float[] norm = new float[3];
+		double[] coord = new double[3];
+		for (int faceIdx = 0; faceIdx < faces; faceIdx++) {
+			g.getNormal(faceIdx*3, norm);
+			w.printf("  facet normal %e %e %e\n", norm[0],norm[1],norm[2]);
+			w.printf("    outer loop\n");
+			g.getCoordinate(faceIdx*3, coord);
+			w.printf("      vertex %e %e %e\n", coord[0],coord[1],coord[2]);
+			g.getCoordinate((faceIdx*3)+1, coord);
+			w.printf("      vertex %e %e %e\n", coord[0],coord[1],coord[2]);
+			g.getCoordinate((faceIdx*3)+2, coord);
+			w.printf("      vertex %e %e %e\n", coord[0],coord[1],coord[2]);
+			w.printf("    endloop\n");
+			w.printf("  endfacet\n");
+		}
+		w.printf("endsolid %s\n", name);
+		w.close();
+	}
+
+}
