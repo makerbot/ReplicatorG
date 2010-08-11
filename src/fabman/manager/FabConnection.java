61b19a263bfac5e9cf13ebd72d71b1266d4fee8f
Adam Mayer <adam@makerbot.com>
2010-08-11 10:15:39 -0400

    Adding protos, fabconnection

--- src/fabman/manager/FabConnection.java
+++ src/fabman/manager/FabConnection.java
@@ -1,5 +1,12 @@
 package fabman.manager;
 
+/**
+ * The FabConnection object represents an open connection to a fabricator
+ * queue.  Fabricator queues run in their own threads.  Communication between
+ * threads is 
+ * @author phooky
+ *
+ */
 public class FabConnection {
 
 }
