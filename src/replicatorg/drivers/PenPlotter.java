c37278363f6e490f68e4f592e0bb7a4e58563b98
Zeeshan <zeeshan.jp@gmail.com>
2010-08-21 07:37:10 +0800

    Committed PenPlotter.java interface class

---
+++ src/replicatorg/drivers/PenPlotter.java
@@ -0,0 +1,15 @@
+package replicatorg.drivers;
+
+
+
+public interface PenPlotter {
+	
+	//public void moveServo(int degree);
+
+	//public void enableServo();
+
+	//public void disableServo();
+
+	public void setServoPos(double degree);
+	
+}
