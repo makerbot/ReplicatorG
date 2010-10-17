d57bc61aef8dd78748b2fb5e815dea76d89903d5
Erik de Bruijn <erik@erik.(none)>
2010-10-17 15:46:27 +0200

    I like to use a more elaborate version number. This allows this. Please use only the version number at the top of changelog.txt

--- dist-mac.sh
+++ dist-mac.sh
@@ -2,7 +2,8 @@
 
 #REVISION=`head -c 4 todo.txt`
 REVISION=`head -n 1 changelog.txt | sed 's/[^0-9]//g'`
+REVISION=`head -n 1 changelog.txt | sed 's/[^0-9a-zA-Z-]//g'`
 
-rm -rf dist
+rm -rf dist/macosx
 ant clean
 ant -Dreplicatorg.version=$REVISION -lib build/macosx/jarbundler-2.1.0.jar dist-macosx
--- dist-windows.sh
+++ dist-windows.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 #REVISION=`head -c 4 todo.txt`
-REVISION=`head -n 1 changelog.txt | sed 's/[^0-9]//g'`
+REVISION=`head -n 1 changelog.txt | sed 's/[^0-9a-zA-Z-]//g'`
 
-rm -rf dist
+rm -rf dist/windows
 ant clean
 ant -Dreplicatorg.version=$REVISION dist-windows
