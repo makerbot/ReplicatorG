667be6ca79ed6794627f49f1f8d825fbf4dc2c1b
Erik de Bruijn <reprap@erikdebruijn.nl>
2010-11-27 20:03:41 +0100

    Allow you to preset the extruder motor's RPM or PWM value with a sensible value that is taken from machines.xml

--- src/replicatorg/app/ui/controlpanel/ExtruderPanel.java
+++ src/replicatorg/app/ui/controlpanel/ExtruderPanel.java
@@ -162,6 +162,7 @@ public class ExtruderPanel extends JPanel implements FocusListener, ActionListen
 				field.setName("motor-speed-pwm");
 				field.addFocusListener(this);
 				field.setActionCommand("handleTextfield");
+				field.setText(Double.toString(t.getMotorSpeedPWM()));
 				field.addActionListener(this);
 
 				add(label);
@@ -184,6 +185,7 @@ public class ExtruderPanel extends JPanel implements FocusListener, ActionListen
 				field.setName("motor-speed");
 				field.addFocusListener(this);
 				field.setActionCommand("handleTextfield");
+				field.setText(Double.toString(t.getMotorSpeedRPM()));
 				field.addActionListener(this);
 
 				add(label);
