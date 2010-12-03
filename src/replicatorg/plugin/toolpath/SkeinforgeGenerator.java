2b66162978dd83f5bcfa613be69ed1fbfaa5da64
phooky <phooky@gmail.com>
2010-12-03 16:26:51 -0500

    * Added choice options to skeinforge * Now choice of support styles

--- src/replicatorg/plugin/toolpath/SkeinforgeGenerator.java
+++ src/replicatorg/plugin/toolpath/SkeinforgeGenerator.java
@@ -6,18 +6,23 @@ import java.awt.event.ActionListener;
 import java.io.File;
 import java.io.IOException;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 import java.util.logging.Level;
 
+import javax.swing.DefaultComboBoxModel;
 import javax.swing.DefaultListModel;
 import javax.swing.JButton;
 import javax.swing.JCheckBox;
+import javax.swing.JComboBox;
 import javax.swing.JComponent;
 import javax.swing.JDialog;
 import javax.swing.JLabel;
 import javax.swing.JList;
 import javax.swing.JOptionPane;
+import javax.swing.JPanel;
 import javax.swing.ListSelectionModel;
 import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
@@ -131,6 +136,62 @@ public abstract class SkeinforgeGenerator extends ToolpathGenerator {
 	protected interface SkeinforgePreference {
 		public JComponent getUI();
 		public List<SkeinforgeOption> getOptions();
+	}
+	
+	protected class SkeinforgeChoicePreference implements SkeinforgePreference {
+		private Map<String,List<SkeinforgeOption>> optionsMap = new HashMap<String,List<SkeinforgeOption>>();
+		private JPanel component;
+		private DefaultComboBoxModel model;
+		private String chosen;
+		
+		SkeinforgeChoicePreference(String name, final String preferenceName, String defaultState, String toolTip) {
+			component = new JPanel(new MigLayout());
+			chosen = defaultState;
+			if (preferenceName != null) {
+				chosen = Base.preferences.get(preferenceName, defaultState);
+			}
+			model = new DefaultComboBoxModel();
+			model.setSelectedItem(chosen);
+			component.add(new JLabel(name));
+			JComboBox cb = new JComboBox(model);
+			component.add(cb);
+			cb.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					chosen = (String)model.getSelectedItem();
+					if (preferenceName != null) {
+						Base.preferences.put(preferenceName,chosen);
+					}
+				}
+			});
+			if (toolTip != null) {
+				component.setToolTipText(toolTip);
+			}
+		}
+		public JComponent getUI() { return component; }
+		
+		public void addOption(String name, SkeinforgeOption o) {
+			if (!optionsMap.containsKey(name)) {
+				model.addElement(name);
+				optionsMap.put(name, new LinkedList<SkeinforgeOption>());
+				if (name.equals(chosen)) {
+					model.setSelectedItem(name);
+				}
+			}
+			List<SkeinforgeOption> list = optionsMap.get(name);
+			list.add(o);
+		}
+
+		public List<SkeinforgeOption> getOptions() {
+			if (optionsMap.containsKey(chosen)) {
+				List<SkeinforgeOption> l = optionsMap.get(chosen);
+				for (SkeinforgeOption o : l) {
+					System.err.println(o.getSpec());
+				}
+				return optionsMap.get(chosen);
+			}
+			return new LinkedList<SkeinforgeOption>();
+		}
+
 	}
 	
 	protected class SkeinforgeBooleanPreference implements SkeinforgePreference {
