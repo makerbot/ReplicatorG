005147f4698e80d1b8381777ab2540a8192cf96d
phooky <phooky@gmail.com>
2010-12-17 16:34:10 -0500

    * Clean up warning on unused import

--- src/replicatorg/app/ui/ControlPanelWindow.java
+++ src/replicatorg/app/ui/ControlPanelWindow.java
@@ -46,7 +46,6 @@ import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JPanel;
-import javax.swing.JSeparator;
 import javax.swing.JTabbedPane;
 import javax.swing.JTextField;
 import javax.swing.SwingUtilities;
