1d6fabbd2348b5b9e59b73f7aa930fa67387c27b
phooky <phooky@gmail.com>
2011-01-26 16:03:28 -0500

    * oh yay for non-weak references in anonymous inner classes

--- src/replicatorg/plugin/toolpath/skeinforge/ConfigurationDialog.java
+++ src/replicatorg/plugin/toolpath/skeinforge/ConfigurationDialog.java
@@ -7,6 +7,7 @@ import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.lang.ref.WeakReference;
 import java.util.List;
 import java.util.logging.Level;
 
@@ -32,16 +33,19 @@ class ConfigurationDialog extends JDialog {
 	JButton newButton = new JButton("Duplicate...");
 	JButton deleteButton = new JButton("Delete");
 	JButton generate = new JButton("Generate...");
-	private SkeinforgeGenerator parentGenerator;
+	private WeakReference<SkeinforgeGenerator> parentGenerator;
+	private WeakReference<List<Profile>> wrProfileList = new WeakReference<List<Profile>>(null);
 	
 	private void loadList(JList list) {
 		list.removeAll();
-		List<Profile> profiles = parentGenerator.getProfiles();
+		wrProfileList = new WeakReference<List<Profile>>(parentGenerator.get().getProfiles());
+		List<Profile> profiles = wrProfileList.get();
 		DefaultListModel model = new DefaultListModel();
 		int i=0;
 		int foundLastProfile = -1;
 		for (Profile p : profiles) {
-			model.addElement(p);
+			model.addElement(p.toString());
+			
 			if(p.toString().equals(Base.preferences.get("lastGeneratorProfileSelected","---")))
 			{
 				Base.logger.fine("Selecting last used element: " + p);
@@ -68,10 +72,16 @@ class ConfigurationDialog extends JDialog {
 		super.setVisible(b);
 		parentGenerator = null;
 	}
-	
-	public ConfigurationDialog(final Frame parent, final SkeinforgeGenerator parentGenerator) {
+
+	final JList prefList = new JList();
+
+	private Profile getListedProfile(int idx) {
+		return wrProfileList.get().get(idx);
+	}
+
+	public ConfigurationDialog(final Frame parent, final SkeinforgeGenerator parentGeneratorIn) {
 		super(parent, true);
-		this.parentGenerator = parentGenerator;
+		parentGenerator = new WeakReference<SkeinforgeGenerator>(parentGeneratorIn);
 		setTitle("Choose a skeinforge profile");
 		setLayout(new MigLayout("aligny top"));
 
@@ -89,7 +99,6 @@ class ConfigurationDialog extends JDialog {
 
 		add(new JLabel("Select a printing profile:"), "wrap");
 
-		final JList prefList = new JList();
 		prefList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
 		prefList.addListSelectionListener(new ListSelectionListener() {
 
@@ -105,14 +114,14 @@ class ConfigurationDialog extends JDialog {
 		
 		// Add a listener for mouse clicks
 		prefList.addMouseListener(new MouseAdapter() {
-		    public void mouseClicked(MouseEvent evt) {
+			public void mouseClicked(MouseEvent evt) {
 		        JList list = (JList)evt.getSource();
 		        if (evt.getClickCount() == 2) { // Double-click generates with this profile
 		            int idx = list.locationToIndex(evt.getPoint());
-					Profile p = (Profile) prefList.getModel().getElementAt(idx);
+		            Profile p = getListedProfile(idx);
 					Base.preferences.put("lastGeneratorProfileSelected",p.toString());
-					parentGenerator.configSuccess = true;
-					parentGenerator.profile = p.getFullPath();
+					parentGenerator.get().configSuccess = true;
+					parentGenerator.get().profile = p.getFullPath();
 					setVisible(false);
 		        }
 		    }
@@ -127,10 +136,10 @@ class ConfigurationDialog extends JDialog {
 				{
 					int idx = prefList.getSelectedIndex();
 					Base.logger.fine("idx="+idx);
-					Profile p = (Profile) prefList.getModel().getElementAt(idx);
+					Profile p = getListedProfile(idx);
 					Base.preferences.put("lastGeneratorProfileSelected",p.toString());
-					parentGenerator.configSuccess = true;
-					parentGenerator.profile = p.getFullPath();
+					parentGenerator.get().configSuccess = true;
+					parentGenerator.get().profile = p.getFullPath();
 					setVisible(false);
 				} else if(e.getKeyCode() == KeyEvent.VK_ESCAPE) {
 					setVisible(false);
@@ -148,9 +157,8 @@ class ConfigurationDialog extends JDialog {
 					JOptionPane.showMessageDialog(parent,
 							"Select a profile to edit.");
 				} else {
-					Profile p = (Profile) prefList.getModel().getElementAt(
-							idx);
-					parentGenerator.editProfile(p);
+					Profile p = getListedProfile(idx);
+					parentGenerator.get().editProfile(p);
 				}
 			}
 		});
@@ -162,9 +170,8 @@ class ConfigurationDialog extends JDialog {
 				String newName = JOptionPane.showInputDialog(parent,
 						"Name your new profile:");
 				if (newName != null) {
-					Profile p = (Profile) prefList.getModel().getElementAt(
-							idx);
-					parentGenerator.duplicateProfile(p, newName);
+					Profile p = getListedProfile(idx);
+					parentGenerator.get().duplicateProfile(p, newName);
 					loadList(prefList);
 					pack();
 				}
@@ -175,7 +182,7 @@ class ConfigurationDialog extends JDialog {
 		deleteButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				int idx = prefList.getSelectedIndex();
-				Profile p = (Profile) prefList.getModel().getElementAt(idx);
+				Profile p = getListedProfile(idx);
 				boolean result = new ProfileUtils().delete(p);
 				loadList(prefList);
 				pack();
@@ -194,20 +201,19 @@ class ConfigurationDialog extends JDialog {
 		generate.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				int idx = prefList.getSelectedIndex();
-				Profile p = (Profile) prefList.getModel().getElementAt(idx);
+				Profile p = getListedProfile(idx);
 				Base.preferences.put("lastGeneratorProfileSelected",p.toString());
-				parentGenerator.configSuccess = true;
-				parentGenerator.profile = p.getFullPath();
+				parentGenerator.get().configSuccess = true;
+				parentGenerator.get().profile = p.getFullPath();
 				setVisible(false);
 				SkeinforgeGenerator.setSelectedProfile(p.toString());
 			}
 		});
 		cancel.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-				parentGenerator.configSuccess = false;
+				parentGenerator.get().configSuccess = false;
 				setVisible(false);
 			}
 		});
 	}
-
 };
