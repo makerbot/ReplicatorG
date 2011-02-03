2050cce53f61630ac33d3ff5323cea4fb9393ca6
Marius Kintel <marius@kintel.net>
2011-02-03 14:26:00 +0100

    Correct cascading

--- skein_engines/generate_skeinforge_profiles.sh
+++ skein_engines/generate_skeinforge_profiles.sh
@@ -8,14 +8,18 @@
 # Makerbot-baseline 
 # +--Thingomatic-baseline
 # |  +--Thingomatic-non-heated
-# |  +--Thingomatic-HBP
-# |  |  +--Thingomatic-HBP-Stepstruder
-# |  +--Thingomatic-ABP
-# |     +--Thingomatic-HBP-Stepstruder
+# |     +--Thingomatic-HBP
+# |     |  +--Thingomatic-HBP-Stepstruder
+# |     +--Thingomatic-ABP
+# |        +--Thingomatic-HBP-Stepstruder
 # +--Cupcake-baseline
-#    +--Cupcake-HBP
-#    +--Cupcake-ABP
+#    +--Thingomatic-non-heated
+#       +--Cupcake-HBP
+#       +--Cupcake-ABP
 # 
+# Example of how to generate new diffs (NB! these must be manually examined before use):
+# diff -P -u -x "*~" -x "\.*" -r SF35-Thingomatic-non-heated SF35-Thingomatic-HBP > HBP.diff
+#
 
 if [ $# != 2 ]; then
   echo "Usage: $0 <skeinforge-version> <diffdir>"
@@ -43,10 +47,11 @@ cp -r SF$VERSION-Thingomatic-baseline SF$VERSION-Thingomatic-non-heated
 echo "Building SF$VERSION-Thingomatic-HBP"
 rm -rf SF$VERSION-Thingomatic-HBP
 cp -r SF$VERSION-Thingomatic-non-heated SF$VERSION-Thingomatic-HBP
+cd SF$VERSION-Thingomatic-HBP; patch -s -p1 < $DIFFS/HBP.diff; cd ..
 
 echo "Building SF$VERSION-Thingomatic-ABP"
 rm -rf SF$VERSION-Thingomatic-ABP
-cp -r SF$VERSION-Thingomatic-HBP SF$VERSION-Thingomatic-ABP
+cp -r SF$VERSION-Thingomatic-non-heated SF$VERSION-Thingomatic-ABP
 cd SF$VERSION-Thingomatic-ABP; patch -s -p1 < $DIFFS/Thingomatic-ABP.diff; cd ..
 
 echo "Building SF$VERSION-Thingomatic-HBP-Stepstruder"
@@ -71,8 +76,9 @@ cp -r SF$VERSION-cupcake-baseline SF$VERSION-cupcake-non-heated
 echo "Building SF$VERSION-cupcake-HBP"
 rm -rf SF$VERSION-cupcake-HBP
 cp -r SF$VERSION-cupcake-baseline SF$VERSION-cupcake-HBP
+cd SF$VERSION-cupcake-HBP; patch -s -p1 < $DIFFS/HBP.diff; cd ..
 
 echo "Building SF$VERSION-cupcake-ABP"
 rm -rf SF$VERSION-cupcake-ABP
-cp -r SF$VERSION-cupcake-HBP SF$VERSION-cupcake-ABP
+cp -r SF$VERSION-cupcake-non-heated SF$VERSION-cupcake-ABP
 cd SF$VERSION-cupcake-ABP; patch -s -p1 < $DIFFS/cupcake-ABP.diff; cd ..
