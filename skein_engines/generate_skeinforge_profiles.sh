f15e66d9edf04f1f7c638086925f5627ee6afdee
Marius Kintel <marius@kintel.net>
2011-02-03 02:43:26 +0100

    Moved scripts to skein_engines folder, added okay->yes patch from Matt

--- skein_engines/generate_skeinforge_profiles.sh
+++ skein_engines/generate_skeinforge_profiles.sh
@@ -11,8 +11,8 @@ DIFFDIR=$2
 echo "Using skeinforge version $VERSION"
 echo "Patching using $DIFFDIR"
 
-SOURCE=$PWD/$DIFFDIR
-cd ../skein_engines/skeinforge-$VERSION/skeinforge_application/prefs
+DIFFS=$PWD/$DIFFDIR
+cd skeinforge-$VERSION/skeinforge_application/prefs
 
 echo "Building SF$VERSION-Thingomatic-non-heated"
 rm -rf SF$VERSION-Thingomatic-non-heated
@@ -25,14 +25,14 @@ cp -r SF$VERSION-Thingomatic-non-heated SF$VERSION-Thingomatic-HBP
 echo "Building SF$VERSION-Thingomatic-ABP"
 rm -rf SF$VERSION-Thingomatic-ABP
 cp -r SF$VERSION-Thingomatic-HBP SF$VERSION-Thingomatic-ABP
-cd SF$VERSION-Thingomatic-ABP; patch -s -p1 < $SOURCE/ABP.diff; cd ..
+cd SF$VERSION-Thingomatic-ABP; patch -s -p1 < $DIFFS/ABP.diff; cd ..
 
 echo "Building SF$VERSION-Thingomatic-HBP-Stepstruder"
 rm -rf SF$VERSION-Thingomatic-HBP-Stepstruder
 cp -r SF$VERSION-Thingomatic-HBP SF$VERSION-Thingomatic-HBP-Stepstruder
-cd SF$VERSION-Thingomatic-HBP-Stepstruder; patch -s -p1 < $SOURCE/Stepstruder.diff; cd ..
+cd SF$VERSION-Thingomatic-HBP-Stepstruder; patch -s -p1 < $DIFFS/Stepstruder.diff; cd ..
 
 echo "Building SF$VERSION-Thingomatic-ABP-Stepstruder"
 rm -rf SF$VERSION-Thingomatic-ABP-Stepstruder
 cp -r SF$VERSION-Thingomatic-ABP SF$VERSION-Thingomatic-ABP-Stepstruder
-cd SF$VERSION-Thingomatic-ABP-Stepstruder; patch -s -p1 < $SOURCE/Stepstruder.diff; cd ..
+cd SF$VERSION-Thingomatic-ABP-Stepstruder; patch -s -p1 < $DIFFS/Stepstruder.diff; cd ..
