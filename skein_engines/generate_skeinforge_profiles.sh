7b46f7a558a9f72a5acb99ea53851ddd742520fb
Marius Kintel <marius@kintel.net>
2011-02-04 15:24:29 +0100

    Created homing section also for cupcakes; homing just sets the origin

--- skein_engines/generate_skeinforge_profiles.sh
+++ skein_engines/generate_skeinforge_profiles.sh
@@ -17,8 +17,13 @@
 #       +--Cupcake-HBP
 #       +--Cupcake-ABP
 # 
-# Example of how to generate new diffs (NB! these must be manually examined before use):
+# Examples of how to generate new diffs (NB! these must be manually examined before use):
+# diff -P -u -x "*~" -x "\.*" -r SF35-Makerbot-baseline SF35-Thingomatic-baseline > Thingomatic.diff
+# diff -P -u -x "*~" -x "\.*" -r SF35-Makerbot-baseline SF35-cupcake-baseline > cupcake.diff
 # diff -P -u -x "*~" -x "\.*" -r SF35-Thingomatic-non-heated SF35-Thingomatic-HBP > HBP.diff
+# diff -P -u -x "*~" -x "\.*" -r SF35-Thingomatic-non-heated SF35-Thingomatic-ABP > Thingomatic-ABP.diff
+# diff -P -u -x "*~" -x "\.*" -r SF35-Thingomatic-HBP SF35-Thingomatic-HBP-Stepstruder > Stepstruder.diff
+# diff -P -u -x "*~" -x "\.*" -r SF35-cupcake-non-heated SF35-cupcake-ABP > cupcake-ABP.diff
 #
 
 if [ $# != 2 ]; then
