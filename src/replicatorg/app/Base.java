2d8746f7503a31812ade5baa76ea15fa0927e046
Ted <Ted@Makerbot.com>
2011-12-08 12:56:16 -0500

    Changed version string.

--- src/replicatorg/app/Base.java
+++ src/replicatorg/app/Base.java
@@ -105,7 +105,7 @@ public class Base {
 	/**
 	 * The textual representation of this version (4 digits, zero padded).
 	 */
-	public static final String VERSION_NAME = String.format("%04d_Alpha",VERSION);
+	public static final String VERSION_NAME = String.format("%04d_Beta",VERSION);
 
 	/**
 	 * The machine controller in use.
