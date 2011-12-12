136ec608fb2131e7546ef3cab5edcece357d4269
Ted <Ted@Makerbot.com>
2011-12-12 11:40:26 -0500

    This should fix a bug that reversed raft setting for SF35.

--- src/replicatorg/plugin/toolpath/ToolpathGeneratorFactory.java
+++ src/replicatorg/plugin/toolpath/ToolpathGeneratorFactory.java
@@ -101,7 +101,7 @@ public class ToolpathGeneratorFactory {
 						"replicatorg.skeinforge.useRaft", false,
 						"If this option is checked, skeinforge will lay down a rectangular 'raft' of plastic before starting the build.  "
 						+ "Rafts increase the build size slightly, so you should avoid using a raft if your build goes to the edge of the platform.");
-				raftPref.addNegateableOption(new SkeinforgeOption("raft.csv", "Add Raft, Elevate Nozzle, Orbit and Set Altitude:", "false"));
+				raftPref.addNegateableOption(new SkeinforgeOption("raft.csv", "Add Raft, Elevate Nozzle, Orbit and Set Altitude:", "true"));
 				prefs.add(raftPref);
 				SkeinforgeChoicePreference supportPref =
 					new SkeinforgeChoicePreference("Use support material",
