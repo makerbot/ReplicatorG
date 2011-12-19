6dd7589eaf8e5c487cb7b6fcab4c39681996fa50
phooky <phooky@gmail.com>
2011-12-19 11:07:39 -0500

    Adding pause support

--- src/replicatorg/app/gcode/GCodeParser.java
+++ src/replicatorg/app/gcode/GCodeParser.java
@@ -544,10 +544,22 @@ public class GCodeParser {
 			// print message
 			commands.add(new replicatorg.drivers.commands.DisplayMessage(gcode.getCodeValue('P'),gcode.getComment()));
 			break;
+		case M71:
+			// User-clearable pause
+			// First send message, if any...
+			if (gcode.getComment().length() > 0) {
+				commands.add(new replicatorg.drivers.commands.DisplayMessage(0,gcode.getComment()));
+			} else {
+				commands.add(new replicatorg.drivers.commands.DisplayMessage(0,"Paused, press button\nto continue"));
+			}
+			// ...then send user pause command. 
+			commands.add(new replicatorg.drivers.commands.UserPause(gcode.getCodeValue('P'),true,0xff));
+			break;
 		case M101:
 			commands.add(new replicatorg.drivers.commands.SetMotorDirection(DriverCommand.AxialDirection.CLOCKWISE));
 			commands.add(new replicatorg.drivers.commands.EnableExtruderMotor());
 			break;
+		case M71:
 		// turn extruder on, reverse
 		case M102:
 			commands.add(new replicatorg.drivers.commands.SetMotorDirection(DriverCommand.AxialDirection.COUNTERCLOCKWISE));
---
+++ src/replicatorg/drivers/commands/UserPause.java
@@ -0,0 +1,33 @@
+package replicatorg.drivers.commands;
+
+import replicatorg.drivers.Driver;
+import replicatorg.drivers.InteractiveDisplay;
+import replicatorg.drivers.RetryException;
+
+public class UserPause implements DriverCommand {
+
+	String message;
+	boolean resetOnTimeout;
+	int buttonMask;
+
+	/** 
+	 * Display a message on the printing device, if supported.
+	 * @param seconds The time, in seconds, to pause for.
+	 * 0 indicates that the pause should be indefinite.
+	 * @param resetOnTimeout true if the machine should reset if
+	 * the pause times out.
+	 * @param buttonMask a bitfield representing the buttons to wait
+	 * on. 0xff indicates that any button press should continue.
+	 */
+	public UserPause(double seconds, boolean resetOnTimeout, int buttonMask) {
+		this.seconds = seconds;
+		this.resetOnTimeout = resetOnTimeout;
+	}
+		
+	@Override
+	public void run(Driver driver) throws RetryException {
+		if (driver instanceof InteractiveDisplay) {
+			((InteractiveDisplay)driver).userPause(seconds,resetOnTimeout);
+		}
+	}
+}
