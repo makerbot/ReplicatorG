makerbot/ReplicatorG

commit 0291db72d76b0b895de596a3dc07f049daa4d26d

Changed the handling of JFormattedTextFields to be consistent across the program, and to use what I think is the right way of parsing numbers. It is possible that something slipped through, though.

src/replicatorg/app/ui/controlpanel/ExtruderPanel.java
@@ -11,10 +11,7 @@ import java.awt.event.FocusListener;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
 import java.awt.image.BufferedImage;
-import java.text.NumberFormat;
-import java.text.ParseException;
 import java.util.Date;
-import java.util.logging.Level;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -25,11 +22,11 @@ import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JCheckBox;
 import javax.swing.JComboBox;
+import javax.swing.JFormattedTextField;
 import javax.swing.JLabel;
-import javax.swing.JPanel;
 import javax.swing.JOptionPane;
+import javax.swing.JPanel;
 import javax.swing.JRadioButton;
-import javax.swing.JTextField;
 import javax.swing.SwingConstants;
 
 import net.miginfocom.swing.MigLayout;
@@ -52,11 +49,11 @@ import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
 import replicatorg.app.Base;
+import replicatorg.app.ui.CallbackTextField;
 import replicatorg.drivers.RetryException;
 import replicatorg.drivers.commands.DriverCommand.AxialDirection;
 import replicatorg.machine.MachineInterface;
 import replicatorg.machine.model.ToolModel;
-import replicatorg.app.ui.CallbackTextField;
 
 public class ExtruderPanel extends JPanel implements FocusListener, ActionListener, ItemListener {
 	private ToolModel toolModel;
@@ -64,9 +61,9 @@ public class ExtruderPanel extends JPanel implements FocusListener, ActionListen
 
 	public ToolModel getTool() { return toolModel; }
 	
-	protected JTextField currentTempField;
+	protected JFormattedTextField currentTempField;
 	
-	protected JTextField platformCurrentTempField;
+	protected JFormattedTextField platformCurrentTempField;
 
 	protected double targetTemperature;
 	protected double targetPlatformTemperature;
@@ -185,9 +182,7 @@ public class ExtruderPanel extends JPanel implements FocusListener, ActionListen
 		this.machine = machine;
 		this.toolModel = t;
 		
-		int textBoxWidth = 75;
 		Dimension panelSize = new Dimension(420, 30);
-//		driver = machine2.getDriver();
 
 		extrudeTimePattern = Pattern.compile("([.0-9]+)");
 		
@@ -201,8 +196,8 @@ public class ExtruderPanel extends JPanel implements FocusListener, ActionListen
 			if ((t.getMotorStepperAxis() == null) && !(t.motorHasEncoder() || t.motorIsStepper())) {
 				// our motor speed vars
 				JLabel label = makeLabel("Motor Speed (PWM)");
-				JTextField field = new CallbackTextField(this, "handleTextField", "motor-speed-pwm", 9);
-				field.setText(Integer.toString(machine.getDriverQueryInterface().getMotorSpeedPWM()));
+				JFormattedTextField field = new CallbackTextField(this, "handleTextField", "motor-speed-pwm", 9, Base.getLocalFormat());
+				field.setValue(Integer.toString(machine.getDriverQueryInterface().getMotorSpeedPWM()));
 				add(label);
 				add(field,"wrap");
 			}
@@ -210,8 +205,8 @@ public class ExtruderPanel extends JPanel implements FocusListener, ActionListen
 			if (t.motorHasEncoder() || t.motorIsStepper()) {
 				// our motor speed vars
 				JLabel label = makeLabel("Motor Speed (RPM)");
-				JTextField field = new CallbackTextField(this, "handleTextField", "motor-speed", 9);
-				field.setText(Double.toString(machine.getDriverQueryInterface().getMotorRPM()));
+				JFormattedTextField field = new CallbackTextField(this, "handleTextField", "motor-speed", 9, Base.getLocalFormat());
+				field.setValue(machine.getDriverQueryInterface().getMotorRPM());
 				add(label);
 				add(field,"wrap");
 
@@ -287,14 +282,15 @@ public class ExtruderPanel extends JPanel implements FocusListener, ActionListen
 		// our temperature fields
 		if (t.hasHeater()) {
 			JLabel targetTempLabel = makeKeyLabel("Target Temperature (C)",targetColor);
-			JTextField targetTempField = new CallbackTextField(this, "handleTextField", "target-temp", 9);
+			JFormattedTextField targetTempField = new CallbackTextField(this, "handleTextField", "target-temp", 9, Base.getLocalFormat());
 			targetTemperature = machine.getDriverQueryInterface().getTemperatureSetting();
-			targetTempField.setText(Double.toString(targetTemperature));
+			targetTempField.setValue(targetTemperature);
 
 			JLabel currentTempLabel = makeKeyLabel("Current Temperature (C)",measuredColor);
-			currentTempField = new JTextField("",9);
+			currentTempField = new JFormattedTextField(Base.getLocalFormat());
+			currentTempField.setColumns(9);
 			currentTempField.setEnabled(false);
-
+			
 			add(targetTempLabel);
 			add(targetTempField,"wrap");
 			add(currentTempLabel);
@@ -304,12 +300,13 @@ public class ExtruderPanel extends JPanel implements FocusListener, ActionListen
 		// our heated platform fields
 		if (t.hasHeatedPlatform()) {
 			JLabel targetTempLabel = makeKeyLabel("Platform Target Temp (C)",targetPlatformColor);
-			JTextField targetTempField = new CallbackTextField(this, "handleTextField", "platform-target-temp", 9);
+			JFormattedTextField targetTempField = new CallbackTextField(this, "handleTextField", "platform-target-temp", 9, Base.getLocalFormat());
 			targetPlatformTemperature = machine.getDriverQueryInterface().getPlatformTemperatureSetting();
-			targetTempField.setText(Double.toString(targetPlatformTemperature));
+			targetTempField.setValue(targetPlatformTemperature);
 
 			JLabel currentTempLabel = makeKeyLabel("Platform Current Temp (C)",measuredPlatformColor);
-			platformCurrentTempField = new JTextField("",9);
+			platformCurrentTempField = new JFormattedTextField(Base.getLocalFormat());
+			platformCurrentTempField.setColumns(9);
 			platformCurrentTempField.setEnabled(false);
 
 			add(targetTempLabel);
@@ -453,14 +450,14 @@ public class ExtruderPanel extends JPanel implements FocusListener, ActionListen
 	
 	synchronized public void updateTemperature(Second second, double temperature)
 	{
-		currentTempField.setText(Double.toString(temperature));
+		currentTempField.setValue(temperature);
 		measuredDataset.add(second, temperature,"a");
 		targetDataset.add(second, targetTemperature,"a");
 	}
 
 	synchronized public void updatePlatformTemperature(Second second, double temperature)
 	{
-		platformCurrentTempField.setText(Double.toString(temperature));
+		platformCurrentTempField.setValue(temperature);
 		measuredPlatformDataset.add(second, temperature,"a");
 		targetPlatformDataset.add(second, targetPlatformTemperature,"a");
 	}
@@ -469,7 +466,7 @@ public class ExtruderPanel extends JPanel implements FocusListener, ActionListen
 	}
 
 	public void focusLost(FocusEvent e) {
-		JTextField source = (JTextField) e.getSource();
+		JFormattedTextField source = (JFormattedTextField) e.getSource();
 		try {
 			handleChangedTextField(source);
 		} catch (RetryException e1) {
@@ -510,19 +507,11 @@ public class ExtruderPanel extends JPanel implements FocusListener, ActionListen
 		}
 	}
 	
-	public void handleChangedTextField(JTextField source) throws RetryException
+	public void handleChangedTextField(JFormattedTextField source) throws RetryException
 	{
 		String name = source.getName();
 		if (source.getText().length() > 0) {
-			double target;
-			try {
-				NumberFormat nf = Base.getLocalFormat();
-				target = nf.parse(source.getText()).doubleValue();
-			} catch (ParseException pe) {
-				Base.logger.log(Level.WARNING,"Could not parse value!",pe);
-				JOptionPane.showMessageDialog(this, "Error parsing value: "+pe.getMessage()+"\nPlease try again.", "Could not parse value", JOptionPane.ERROR_MESSAGE);
-				return;
-			}
+			double target = ((Number)source.getValue()).doubleValue();
 			if (name.equals("target-temp") || name.equals("platform-target-temp")) {
 				
 				if(name.equals("target-temp")) {
@@ -541,7 +530,7 @@ public class ExtruderPanel extends JPanel implements FocusListener, ActionListen
 					targetPlatformTemperature = target;
 				}
 				// This gives some feedback by adding .0 if it wasn't typed.
-				source.setText(Double.toString(target));
+				source.setValue(target);
 			} else if (name.equals("motor-speed")) {
 				machine.runCommand(new replicatorg.drivers.commands.SetMotorSpeedRPM(target));
 			} else if (name.equals("motor-speed-pwm")) {
@@ -622,7 +611,7 @@ public class ExtruderPanel extends JPanel implements FocusListener, ActionListen
 		
 		if(s.equals("handleTextField"))
 		{
-			JTextField source = (JTextField) e.getSource();
+			JFormattedTextField source = (JFormattedTextField) e.getSource();
 			try {
 				handleChangedTextField(source);
 			} catch (RetryException e1) {

src/replicatorg/app/Base.java
@@ -216,13 +216,13 @@ public class Base {
 
 	/** Local storage for localized NumberFormat. */
 	static private NumberFormat localNF = NumberFormat.getInstance();
-	{
-		localNF.setMinimumFractionDigits(2);
-	}
+//	{
+//		localNF.setMinimumFractionDigits(2);
+//	}
 	
 	/**
 	 * Get the NumberFormat object used for parsing and displaying numbers in the localized
-	 * format. This should be used for all non-GCode input and output.
+	 * format. This should be used for all non-GCode, floating point input and output.
 	 */
 	static public NumberFormat getLocalFormat() {
 		return localNF;

src/replicatorg/app/ui/ActionTextField.java
@@ -6,13 +6,19 @@ import java.awt.event.FocusEvent;
 import java.awt.event.FocusListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.text.Format;
+import java.text.ParseException;
+import java.util.logging.Level;
 
-import javax.swing.JTextField;
+import javax.swing.JFormattedTextField;
+import javax.swing.text.DefaultFormatter;
+
+import replicatorg.app.Base;
 
 
 // Text field that keeps track of whether it's data has been modified, and calls a function
 // when it loses focus or gets an ENTER key to allow the subclasser to handle the event.
-public abstract class ActionTextField extends JTextField {
+public abstract class ActionTextField extends JFormattedTextField {
 	Color defaultColor;
 	Color modifiedColor;
 	
@@ -94,8 +100,16 @@ public abstract class ActionTextField extends JTextField {
 	
 	public abstract void doSaveEvent();
 	
-	public ActionTextField(String text, int columns) {
-		super(text, columns);
+	public ActionTextField(Object value, int columns, Format format) {
+//		super(text, columns);
+		super(format);
+		setColumns(columns);
+		
+		if(format == null)
+			super.setFormatter(new DefaultFormatter());
+		
+		if(value != null)
+			setValue(value);
 		
 		Toolkit tk = Toolkit.getDefaultToolkit();
 		defaultColor = this.getBackground();

src/replicatorg/app/ui/CallbackTextField.java
@@ -1,7 +1,7 @@
 package replicatorg.app.ui;
 
-import java.awt.Dimension;
 import java.awt.event.ActionEvent;
+import java.text.Format;
 
 import replicatorg.app.ui.controlpanel.ExtruderPanel;
 
@@ -9,8 +9,8 @@ public class CallbackTextField extends ActionTextField {
 	ExtruderPanel panel;
 	String actionCommand;
 	
-	public CallbackTextField(ExtruderPanel panel, String actionCommand, String name, int columns) {
-		super("", columns);
+	public CallbackTextField(ExtruderPanel panel, String actionCommand, String name, int columns, Format format) {
+		super(null, columns, format);
 		
 //		setMaximumSize(new Dimension(textBoxWidth, 25));
 //		setMinimumSize(new Dimension(textBoxWidth, 25));

src/replicatorg/app/ui/ExtruderOnboardParameters.java
@@ -5,11 +5,9 @@ import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.text.NumberFormat;
-import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Vector;
-import java.util.logging.Level;
 
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
@@ -86,17 +84,10 @@ public class ExtruderOnboardParameters extends JFrame {
 		}
 
 		public void commit() {
-			NumberFormat nf = Base.getLocalFormat();
-			try {
-				int beta = nf.parse(betaField.getText()).intValue();
-				int r0 = nf.parse(r0Field.getText()).intValue();
-				int t0 = nf.parse(t0Field.getText()).intValue();
-				target.createThermistorTable(which,r0,t0,beta,tool.getIndex());
-			} catch (ParseException pe) {
-				Base.logger.log(Level.WARNING,"Could not parse value!",pe);
-				JOptionPane.showMessageDialog(this, "Error parsing value: "+pe.getMessage()+"\nPlease try again.", "Could not parse value", JOptionPane.ERROR_MESSAGE);
-				return;
-			}
+			int beta = ((Number)betaField.getValue()).intValue();
+			int r0 = ((Number)r0Field.getValue()).intValue();
+			int t0 = ((Number)t0Field.getValue()).intValue();
+			target.createThermistorTable(which,r0,t0,beta,tool.getIndex());
 		}
 		
 		public boolean isCommitable() {
@@ -159,19 +150,12 @@ public class ExtruderOnboardParameters extends JFrame {
 		}
 
 		public void commit() {
-			NumberFormat nf = Base.getLocalFormat();
 			OnboardParameters.BackoffParameters bp = new OnboardParameters.BackoffParameters();
-			try {
-				bp.forwardMs = nf.parse(forwardMsField.getText()).intValue();
-				bp.reverseMs = nf.parse(reverseMsField.getText()).intValue();
-				bp.stopMs = nf.parse(stopMsField.getText()).intValue();
-				bp.triggerMs = nf.parse(triggerMsField.getText()).intValue();
-				target.setBackoffParameters(bp, tool.getIndex());
-			} catch (ParseException pe) {
-				Base.logger.log(Level.WARNING,"Could not parse value!",pe);
-				JOptionPane.showMessageDialog(this, "Error parsing value: "+pe.getMessage()+"\nPlease try again.", "Could not parse value", JOptionPane.ERROR_MESSAGE);
-				return;
-			}
+			bp.forwardMs = ((Number)forwardMsField.getValue()).intValue();
+			bp.reverseMs = ((Number)reverseMsField.getValue()).intValue();
+			bp.stopMs = ((Number)stopMsField.getValue()).intValue();
+			bp.triggerMs = ((Number)triggerMsField.getValue()).intValue();
+			target.setBackoffParameters(bp, tool.getIndex());
 		}
 		
 		public boolean isCommitable() {
@@ -267,17 +251,10 @@ public class ExtruderOnboardParameters extends JFrame {
 
 		public void commit() {
 			OnboardParameters.PIDParameters pp = new OnboardParameters.PIDParameters();
-			NumberFormat nf = Base.getLocalFormat();
-			try{ 
-				pp.p = nf.parse(pField.getText()).floatValue();
-				pp.i = nf.parse(iField.getText()).floatValue();
-				pp.d = nf.parse(dField.getText()).floatValue();
-				target.setPIDParameters(which,pp, tool.getIndex());
-			} catch (ParseException pe) {
-				Base.logger.log(Level.WARNING,"Could not parse value!",pe);
-				JOptionPane.showMessageDialog(this, "Error parsing value: "+pe.getMessage()+"\nPlease try again.", "Could not parse value", JOptionPane.ERROR_MESSAGE);
-				return;
-			}
+			pp.p = ((Number)pField.getValue()).floatValue();
+			pp.i = ((Number)iField.getValue()).floatValue();
+			pp.d = ((Number)dField.getValue()).floatValue();
+			target.setPIDParameters(which,pp, tool.getIndex());
 		}
 		
 		public boolean isCommitable() {
@@ -310,16 +287,9 @@ public class ExtruderOnboardParameters extends JFrame {
 		}
 
 		public void commit() {
-			NumberFormat nf = Base.getLocalFormat();
 			boolean enabled = coolingFanEnabled.isSelected();   
-			try {
-				int setpoint = nf.parse(coolingFanSetpoint.getText()).intValue();
-				target.setCoolingFanParameters(enabled, setpoint, tool.getIndex());
-			} catch (ParseException pe) {
-				Base.logger.log(Level.WARNING,"Could not parse value!",pe);
-				JOptionPane.showMessageDialog(this, "Error parsing value: "+pe.getMessage()+"\nPlease try again.", "Could not parse value", JOptionPane.ERROR_MESSAGE);
-				return;
-			}
+			int setpoint = ((Number)coolingFanSetpoint.getValue()).intValue();
+			target.setCoolingFanParameters(enabled, setpoint, tool.getIndex());
 		}
 		
 		public boolean isCommitable() {

src/replicatorg/app/ui/MachineOnboardParameters.java
@@ -65,11 +65,11 @@ public class MachineOnboardParameters extends JFrame {
     {
         threePlaces.setMaximumFractionDigits(3);
     }
-	private JFormattedTextField xAxisHomeOffsetField = new JFormattedTextField(threePlaces );
-	private JFormattedTextField yAxisHomeOffsetField = new JFormattedTextField(threePlaces );
-	private JFormattedTextField zAxisHomeOffsetField = new JFormattedTextField(threePlaces );
-	private JFormattedTextField aAxisHomeOffsetField = new JFormattedTextField(threePlaces );
-	private JFormattedTextField bAxisHomeOffsetField = new JFormattedTextField(threePlaces );
+	private JFormattedTextField xAxisHomeOffsetField = new JFormattedTextField(threePlaces);
+	private JFormattedTextField yAxisHomeOffsetField = new JFormattedTextField(threePlaces);
+	private JFormattedTextField zAxisHomeOffsetField = new JFormattedTextField(threePlaces);
+	private JFormattedTextField aAxisHomeOffsetField = new JFormattedTextField(threePlaces);
+	private JFormattedTextField bAxisHomeOffsetField = new JFormattedTextField(threePlaces);
 	
 	private void resetDialog() {
 		int confirm = JOptionPane.showConfirmDialog(this, 
@@ -108,18 +108,11 @@ public class MachineOnboardParameters extends JFrame {
 			target.setEstopConfig(estop);
 		}
 		
-		try {
-			NumberFormat nf = Base.getLocalFormat();
-			target.setAxisHomeOffset(0, nf.parse(xAxisHomeOffsetField.getText()).doubleValue());
-			target.setAxisHomeOffset(1, nf.parse(yAxisHomeOffsetField.getText()).doubleValue());
-			target.setAxisHomeOffset(2, nf.parse(zAxisHomeOffsetField.getText()).doubleValue());
-			target.setAxisHomeOffset(3, nf.parse(aAxisHomeOffsetField.getText()).doubleValue());
-			target.setAxisHomeOffset(4, nf.parse(bAxisHomeOffsetField.getText()).doubleValue());
-		} catch (ParseException pe) {
-			Base.logger.log(Level.WARNING,"Could not parse value!",pe);
-			JOptionPane.showMessageDialog(this, "Error parsing value: "+pe.getMessage()+"\nPlease try again.", "Could not parse value", JOptionPane.ERROR_MESSAGE);
-			return;
-		}
+		target.setAxisHomeOffset(0, ((Number)xAxisHomeOffsetField.getValue()).doubleValue());
+		target.setAxisHomeOffset(1, ((Number)yAxisHomeOffsetField.getValue()).doubleValue());
+		target.setAxisHomeOffset(2, ((Number)zAxisHomeOffsetField.getValue()).doubleValue());
+		target.setAxisHomeOffset(3, ((Number)aAxisHomeOffsetField.getValue()).doubleValue());
+		target.setAxisHomeOffset(4, ((Number)bAxisHomeOffsetField.getValue()).doubleValue());
 		resetDialog();
 	}
 

src/replicatorg/app/ui/PreferencesWindow.java
@@ -370,7 +370,7 @@ public class PreferencesWindow extends JFrame implements GuiConstants {
 					if(ae.getSource() == t0Field)
 					{
 						// casting to long because that's what it is
-						target = (Long)t0Field.getValue();
+						target = ((Number)t0Field.getValue()).doubleValue();
 						target = confirmTemperature(target,"temperature.acceptedLimit",200.0);
 						if (target == Double.MIN_VALUE) {
 							return;
@@ -380,7 +380,7 @@ public class PreferencesWindow extends JFrame implements GuiConstants {
 					else if(ae.getSource() == t1Field)
 					{
 						// casting to long because that's what it is
-						target = (Long)t1Field.getValue();
+						target = ((Number)t1Field.getValue()).doubleValue();
 						target = confirmTemperature(target,"temperature.acceptedLimit",200.0);
 						if (target == Double.MIN_VALUE) {
 							return;
@@ -390,7 +390,7 @@ public class PreferencesWindow extends JFrame implements GuiConstants {
 					else if(ae.getSource() == pField)
 					{
 						// casting to long because that's what it is
-						target = (Long)pField.getValue();
+						target = ((Number)pField.getValue()).doubleValue();
 						target = confirmTemperature(target,"temperature.acceptedLimit.bed",90.0);
 						if (target == Double.MIN_VALUE) {
 							return;

src/replicatorg/app/ui/SavingTextField.java
@@ -6,7 +6,7 @@ public class SavingTextField extends ActionTextField {
 	final String parameterName;
 	
 	public SavingTextField(String parameterName, String text, int columns) {
-		super(text, columns);
+		super(text, columns, null);
 
 		this.parameterName = parameterName;
 	}

commit 26f0d68205730b64e3613249e928ac2660bd95c6

Control panel does not open - setValue expects an Integer, not a String.
Probably broken in commit 0291db72d

See http://replicatorg.lighthouseapp.com/projects/66325/tickets/264

src/replicatorg/app/ui/controlpanel/ExtruderPanel.java
@@ -197,7 +197,7 @@ public class ExtruderPanel extends JPanel implements FocusListener, ActionListen
 				// our motor speed vars
 				JLabel label = makeLabel("Motor Speed (PWM)");
 				JFormattedTextField field = new CallbackTextField(this, "handleTextField", "motor-speed-pwm", 9, Base.getLocalFormat());
-				field.setValue(Integer.toString(machine.getDriverQueryInterface().getMotorSpeedPWM()));
+				field.setValue(machine.getDriverQueryInterface().getMotorSpeedPWM());
 				add(label);
 				add(field,"wrap");
 			}