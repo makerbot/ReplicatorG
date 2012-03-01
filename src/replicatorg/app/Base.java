008b99481f815272911421458e65b603febfb88f
Far McKon <FarMcKon@gmail.com>
2012-03-01 10:53:19 -0500

    updated version number for release branch

--- src/replicatorg/app/Base.java
+++ src/replicatorg/app/Base.java
@@ -105,7 +105,7 @@ public class Base {
 	/**
 	 * The textual representation of this version (4 digits, zero padded).
 	 */
-	public static final String VERSION_NAME = String.format("%04d",VERSION) + "-Beta";
+	public static final String VERSION_NAME = String.format("%04d",VERSION);
 
 	/**
 	 * The machine controller in use.
