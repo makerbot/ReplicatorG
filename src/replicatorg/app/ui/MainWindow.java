193e7550429e50e6c8c8956eed56cb5c4dc019fe
Bill Canfield <wdc.rith@gmail.com>
2012-11-01 17:08:13 -0300

    Update src/replicatorg/app/ui/MainWindow.java

--- src/replicatorg/app/ui/MainWindow.java
+++ src/replicatorg/app/ui/MainWindow.java
@@ -2283,8 +2283,8 @@ ToolpathGenerator.GeneratorListener
     ExtensionFilter x3gFilter;
 
     if ((machineLoader.getMachineInterface().getMachineType() == MachineType.THE_REPLICATOR) || (machineLoader.getMachineInterface().getMachineType() == MachineType.REPLICATOR_2)){
-      s3gFilter = new ExtensionFilter(".s3g"," .s3g  (For use with firmware v6.1 or earlier)");
-      x3gFilter = new ExtensionFilter(".x3g"," .x3g  (For use with firmware v6.2 or later)");
+      s3gFilter = new ExtensionFilter(".s3g"," .s3g  (For use with firmware earlier than v7.0)");
+      x3gFilter = new ExtensionFilter(".x3g"," .x3g  (For use with firmware later than v7.0)");
     }
     else{
       s3gFilter = new ExtensionFilter(".s3g"," .s3g  (For use with firmware v3.5 or earlier)");
